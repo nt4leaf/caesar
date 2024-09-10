2428b2e4802227d87915728e96449dce046958dd
Tuan Nguyen <153607937+nt4leaf@users.noreply.github.com>
2024-09-10 14:51:49 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,10 +1,25 @@
 import streamlit as st
 
+st.text("Encode:")
+string_encode = st.text_input('Encode string:')
+step = st.number_input('Input step:', step = 1)
+data_encode = list(string_encode)
+for i in range(len(data_encode)):
+  if data_encode[i].isspace():
+      continue
+  if data_encode[i].islower():
+    data_encode[i] = chr((ord(data_encode[i]) + step - 97) % 26 + 97)
+  if data_encode[i].isupper():
+    data_encode[i] = chr((ord(data_encode[i]) + step - 65) % 26 + 65)
+result_encode = ''.join(data_encode)
+st.text("Encode: ")
+st.write(result_encode)
+
 st.text("Decode:")
 string_decode = st.text_input('Decode string:')
 data_decode = list(string_decode)
 result_decode = []
-for k in range(2):
+for k in range(25):
   for i in range(len(data_decode)):
     if data_decode[i].isspace():
       continue
