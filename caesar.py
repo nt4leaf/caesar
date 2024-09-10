3739b915d7e7eedb5e61344e862e2f67c5f04101
Tuan Nguyen <153607937+nt4leaf@users.noreply.github.com>
2024-09-10 14:47:56 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -4,16 +4,16 @@ st.text("Decode:")
 string_decode = st.text_input('Decode string:')
 data_decode = list(string_decode)
 result_decode = []
-#for k in range(1,25):
-for i in range(len(data_decode)):
-  if data_decode[i].isspace():
-    continue
-  if data_decode[i].islower():
-    data_decode[i] = chr((ord(data_decode[i]) - 72) % 26 + 97)
-  if data_decode[i].isupper():
-    data_decode[i] = chr((ord(data_decode[i]) - 40) % 26 + 65)
-data_decode = ''.join(data_decode)
-result = ''.join(data_decode)
+for k in range(2):
+  for i in range(len(data_decode)):
+    if data_decode[i].isspace():
+      continue
+    if data_decode[i].islower():
+      data_decode[i] = chr((ord(data_decode[i]) - 72) % 26 + 97)
+    if data_decode[i].isupper():
+      data_decode[i] = chr((ord(data_decode[i]) - 40) % 26 + 65)
+  data_decode = ''.join(data_decode)
+  result = ''.join(data_decode)
 result_decode.append(result)
 st.text("Result: ")
 st.write(result_decode)
