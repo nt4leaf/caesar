1ab1fea34a9a4ddf2daf3f543ad43dff1c1b3e1b
Tuan Nguyen <153607937+nt4leaf@users.noreply.github.com>
2024-09-10 14:21:00 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 my_string = st.text_input('Input string:')
-step = 1
+step = st.number_input('Input step:')
 data = list(my_string)
 for i in range(len(data)):
   if data[i].isspace():
       continue
   if data[i].islower():
-    data[i] = chr((ord(data[i]) - 97) % 26 + step + 97)
+    data[i] = chr((ord(data[i]) + step - 97) % 26 + 97)
   if data[i].isupper():
-    data[i] = chr((ord(data[i]) - 65) % 26 + step + 65)
+    data[i] = chr((ord(data[i]) + step - 65) % 26 + 65)
 result = ''.join(data)
-st.write(result)
+st.write("Result: "result)
