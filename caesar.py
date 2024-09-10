2577bd01289ff3f5d38ea2c6ed0e13979d7dd0dc
Tuan Nguyen <153607937+nt4leaf@users.noreply.github.com>
2024-09-10 14:23:25 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 my_string = st.text_input('Input string:')
-step = st.number_input('Input step:')
+step = st.number_input('Input step:', step=1)
 data = list(my_string)
 for i in range(len(data)):
   if data[i].isspace():
