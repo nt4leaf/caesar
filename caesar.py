0dd94ca5716fe5dc7c44e4c6b4d6841fce336e5a
Tuan Nguyen <153607937+nt4leaf@users.noreply.github.com>
2024-09-10 14:45:58 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,32 +1,17 @@
 import streamlit as st
 
-st.text("Encode:")
-string_encode = st.text_input('Encode string:')
-step = st.number_input('Input step:', step = 1)
-data_encode = list(string_encode)
-for i in range(len(data_encode)):
-  if data_encode[i].isspace():
-      continue
-  if data_encode[i].islower():
-    data_encode[i] = chr((ord(data_encode[i]) + step - 97) % 26 + 97)
-  if data_encode[i].isupper():
-    data_encode[i] = chr((ord(data_encode[i]) + step - 65) % 26 + 65)
-result_encode = ''.join(data_encode)
-st.text("Result: ")
-st.write(result_encode)
-
 st.text("Decode:")
 string_decode = st.text_input('Decode string:')
 data_decode = list(string_decode)
 result_decode = []
-for k in range(1,25):
+#for k in range(1,25):
   for i in range(len(data_decode)):
     if data_decode[i].isspace():
         continue
     if data_decode[i].islower():
-      data_decode[i] = chr((ord(data_decode[i]) + step - 98) % 26 + 97)
+      data_decode[i] = chr((ord(data_decode[i]) + step - 72) % 26 + 97)
     if data_decode[i].isupper():
-      data_decode[i] = chr((ord(data_decode[i]) + step - 66) % 26 + 65)
+      data_decode[i] = chr((ord(data_decode[i]) + step - 40) % 26 + 65)
       data_decode = ''.join(data_decode)
   result = ''.join(data_decode)
   result_decode.append(result)
