b27ba1b43a56a5f816d22dd51901375453a7be28
Tuan Nguyen <153607937+nt4leaf@users.noreply.github.com>
2024-09-16 13:14:41 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -3,32 +3,35 @@ st.caption("Written by Nguyen Tuan")
 st.header("Encode:")
 string_encode = ''
 string_encode = st.text_input('Encode string:')
-step_encode = st.number_input('Input step:', step = 1)
+step_encode = st.number_input('Input step-encode:', step = 1)
 data_encode = list(string_encode)
-for i in range(len(data_encode)):
-  if data_encode[i].isspace():
-      continue
-  if data_encode[i].islower():
-    data_encode[i] = chr((ord(data_encode[i]) + step_encode - 97) % 26 + 97)
-  if data_encode[i].isupper():
-    data_encode[i] = chr((ord(data_encode[i]) + step_encode - 65) % 26 + 65)
-result_encode = ''.join(data_encode)
 if string_encode != '':
+  for i in range(len(data_encode)):
+    if data_encode[i].isspace():
+        continue
+    if data_encode[i].islower():
+      data_encode[i] = chr((ord(data_encode[i]) + step_encode - 97) % 26 + 97)
+    if data_encode[i].isupper():
+      data_encode[i] = chr((ord(data_encode[i]) + step_encode - 65) % 26 + 65)
+  result_encode = ''.join(data_encode)
   st.text("Result: ")
   st.write(result_encode)
 
 st.header("Decode:")
 string_decode = ''
 string_decode = st.text_input('Decode string:')
-step_decode = st.number_input('Input step:', step = 1)
+step_decode = st.number_input('Input step-decode:', step = 1)
 data_decode = list(string_decode)
 result_decode = []
-for i in range(len(data_decode)):
-  if data_decode[i].isspace():
-      continue
-  if data_decode[i].islower():
-    data_decode[i] = chr((ord(data_decode[i]) + step_decode - 97) % 26 + 97)
-  if data_decode[i].isupper():
-    data_decode[i] = chr((ord(data_decode[i]) + step_decode - 65) % 26 + 65)
-result_decode = ''.join(data_decode)
+if string_decode != '':
+  for i in range(len(data_decode)):
+    if data_decode[i].isspace():
+        continue
+    if data_decode[i].islower():
+      data_decode[i] = chr((ord(data_decode[i]) + step_decode - 97) % 26 + 97)
+    if data_decode[i].isupper():
+      data_decode[i] = chr((ord(data_decode[i]) + step_decode - 65) % 26 + 65)
+  result_decode = ''.join(data_decode)
+  st.text("Result: ")
+  st.write(result_decode)
 
